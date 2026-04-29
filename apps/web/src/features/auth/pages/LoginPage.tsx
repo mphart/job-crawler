26e6ee0e03c276102d13c1ffc56c3da72aa51650
mphart <mhart5892@gmail.com>
2026-04-28 23:11:35 -0500

    small refactor

--- apps/web/src/features/auth/pages/LoginPage.tsx
+++ apps/web/src/features/auth/pages/LoginPage.tsx
@@ -2,4 +2,12 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { PageShell } from "../../../shared/components/PageShell";
 import { LoginForm } from "../components/LoginForm";
-export function LoginPage(){const navigate=useNavigate();return <PageShell title="Login"><LoginForm onSuccess={()=>navigate("/feed")}/><p style={{marginTop:"0.75rem"}}>Don't have an account? <Link to="/signup">Sign up</Link></p></PageShell>;}
+export function LoginPage(){
+    const navigate=useNavigate();
+    return <PageShell title="Login">
+        <LoginForm onSuccess={()=>navigate("/feed")}/>
+            <p style={{marginTop:"0.75rem"}}>Don't have an account? 
+                <Link to="/signup">Sign up</Link>
+            </p>
+    </PageShell>;
+}
